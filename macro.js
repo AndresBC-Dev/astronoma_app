c4a635cc19949c5188e21e9ce32bfc050c9da16f
Andres buitrago <andresjavier87@hotmail.com>
2024-12-14 13:08:43 -0500

    first commit

---
+++ macro.js
@@ -0,0 +1,1 @@
+import calculadorEdad from "./script";
---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function calculadorEdad(edad){
+    if (edad == 0 && edad <0){
+        console.log("Eres mayor de edad!")
+    }else if{
+        console.log("Eres menor de edad!")
+    }
+}
+
+
+
+calculadorEdad(15);
+
+
+
+//por ahora todo bien!
+export default calculadorEdad;//Calculador iva que hace cosas
